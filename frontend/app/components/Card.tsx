8b73f6de64c0e5fdd5d5a65cc62a0b4e573046c8
Discokatten <isabelle.wincrantz@gmail.com>
2026-09-17 10:30:42 +0200

    added create/join conversation-button on match-cards

--- frontend/app/components/Card.tsx
+++ frontend/app/components/Card.tsx
@@ -1,21 +1,41 @@
-import Wrapper from './Wrapper';
+import Link from 'next/link';
+import Wrapper from '@/app/components/Wrapper';
 
 interface CardProps {
   round: string;
   team1: string;
   team2: string;
+  id: number;
   date: string;
+  isAuthenticated: boolean;
+  hasConversation: boolean;
 }
-export default function Card({ round, team1, team2, date }: CardProps) {
+export default function Card({
+  round,
+  team1,
+  team2,
+  id,
+  date,
+  isAuthenticated = false,
+  hasConversation = false,
+}: CardProps) {
   return (
-    <Wrapper>
-      <div className='text-sm flex flex-row justify-between'>
-        <p>Omgång {round}</p>
-        <p>{date}</p>
-      </div>
-      <p>
-        {team1} vs {team2}
-      </p>
-    </Wrapper>
+    <div className='flex flex-row'>
+      <Wrapper>
+        <div className='text-sm flex flex-row justify-between'>
+          <p>Omgång {round}</p>
+          <p>{date}</p>
+        </div>
+        <p>
+          {team1} vs {team2}
+        </p>
+      </Wrapper>
+      <Link
+        href={isAuthenticated ? `/messages/${id}` : '/login'}
+        className='dark:bg-primary-dark bg-primary/50 rounded-full p-3 text-center mb-auto mt-auto text-sm font-semibold'
+      >
+        {hasConversation ? 'Gå med i konversation' : 'Skapa konversation'}
+      </Link>
+    </div>
   );
 }
--- frontend/app/components/Wrapper.tsx
+++ frontend/app/components/Wrapper.tsx
@@ -3,9 +3,13 @@ type Props = {
   footer?: React.ReactNode;
 };
 
-export default function hWrapper({ children, footer }: Props) {
+export default function Wrapper({ children, footer }: Props) {
   return (
-    <div className='m-4 p-4 grid w-full max-w-sm gap-4 rounded-2xl border border-primary-dark/15 bg-surface shadow-md shadow-primary-dark/10'>
+    <div
+      className={
+        'm-4 grid w-full max-w-sm gap-4 rounded-2xl border border-primary-dark/15 bg-surface p-4 shadow-md shadow-primary-dark/10'
+      }
+    >
       {children}
       <div className='border-t border-muted/30 pt-4 text-center text-sm text-muted'>
         {footer}
